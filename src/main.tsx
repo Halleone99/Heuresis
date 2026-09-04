2bed8f30a5f4bf4b7ca1901bf99980e27ea34abf
Halleone99 <leone.halling@gmail.com>
2026-09-04 23:23:15 +0200

    Route desktop popup windows to the Heuresis cosmos

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import CosmosWindow from "./components/CosmosWindow";
 import "./styles.css";
 import "./standalone-v2.css";
 import "./settings-v2.css";
 
+const params = new URLSearchParams(window.location.search);
+const content = params.get("cosmos") === "1" ? <CosmosWindow /> : <App />;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>{content}</React.StrictMode>,
 );
