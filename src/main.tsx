dc1e17c3df316e30802f0531b4574712beb20b7f
Halleone99 <leone.halling@gmail.com>
2026-09-04 23:26:47 +0200

    Load the Heuresis Greek eta brand styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import CosmosWindow from "./components/CosmosWindow";
 import "./styles.css";
 import "./standalone-v2.css";
 import "./settings-v2.css";
+import "./brand.css";
 
 const params = new URLSearchParams(window.location.search);
 const content = params.get("cosmos") === "1" ? <CosmosWindow /> : <App />;
