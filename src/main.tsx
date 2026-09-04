176df9ef9f89f2b93d6cb286cbc832e835e26a85
Halleone99 <leone.halling@gmail.com>
2026-09-04 20:49:07 +0200

    Load desktop Heuresis product styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./standalone-v2.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
