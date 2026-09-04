5eb88710ac2e4a6a7baeb8e94f57f0f665242f4c
Halleone99 <leone.halling@gmail.com>
2026-09-04 20:53:13 +0200

    Load full Heuresis settings styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./standalone-v2.css";
+import "./settings-v2.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
